3805a096442684dbc64c5ae6d29a3209ad4c6749
AdrianARPM <164609550+AdrianARPM@users.noreply.github.com>
2025-12-16 13:49:24 +0700

    Implement login and logout functions in auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,22 @@
+function login(){
+  const username = document.getElementById("username").value.trim();
+  fetch(API_URL + "?action=login", {
+    method:"POST",
+    body:JSON.stringify({username})
+  })
+  .then(r=>r.json())
+  .then(res=>{
+    if(res.success){
+      localStorage.setItem("user", JSON.stringify(res));
+      location.href="app.html";
+    } else {
+      document.getElementById("msg").innerText="Login gagal";
+    }
+  });
+}
+
+function logout(){
+  localStorage.clear();
+  location.href="index.html";
+}
 
